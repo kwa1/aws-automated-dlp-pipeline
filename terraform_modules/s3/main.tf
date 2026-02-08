fafa1104dfe8285a1019424bc419064c8d2d5630
Dennis Amankwa <amankwa32@hotmail.com>
2026-02-08 11:51:48 +0000

    Add S3 bucket with encryption and access controls
    Define S3 bucket with encryption and public access settings.

---
+++ terraform_modules/s3/main.tf
@@ -0,0 +1,26 @@
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = var.kms_key_id
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket                  = aws_s3_bucket.this.id
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
+
+output "bucket" {
+  value = aws_s3_bucket.this.id
+}
