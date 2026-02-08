5c4fcf09c7a2e8e32d4b507806b5a13b20cc94c8
Dennis Amankwa <amankwa32@hotmail.com>
2026-02-08 12:14:51 +0000

    Add AWS Cost Anomaly Detector for Macie alerts

--- terraform_modules/cost-control/main.tf
+++ terraform_modules/cost-control/main.tf
@@ -28,3 +28,32 @@ resource "aws_budgets_budget" "dlp_monthly" {
     subscriber_email_addresses = var.alert_emails
   }
 }
+# Macie Cost Anomaly Detector
+resource "aws_ce_anomaly_monitor" "macie" {
+  name              = "macie-cost-anomaly"
+  monitor_type      = "DIMENSIONAL"
+
+  monitor_dimension = "SERVICE"
+}
+# Alert Subscription
+resource "aws_ce_anomaly_subscription" "macie_alerts" {
+  name      = "macie-anomaly-alerts"
+  frequency = "IMMEDIATE"
+
+  monitor_arn_list = [
+    aws_ce_anomaly_monitor.macie.arn
+  ]
+
+  subscribers {
+    type    = "EMAIL"
+    address = var.alert_emails[0]
+  }
+
+  threshold_expression {
+    dimension {
+      key           = "SERVICE"
+      values        = ["Amazon Macie"]
+      match_options = ["EQUALS"]
+    }
+  }
+}
