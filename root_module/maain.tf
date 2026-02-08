72e5dc4e4c15594c25b169b7e6e595a2194150ed
Dennis Amankwa <amankwa32@hotmail.com>
2026-02-08 10:30:00 +0000

    Add initial Terraform configuration for AWS modules
    Initialize Terraform configuration with AWS provider and multiple modules for KMS, S3, Macie, IAM, Lambda remediation, EventBridge, S3 DLP, CloudTrail, GuardDuty, and Security Hub.

---
+++ root_module/maain.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_caller_identity" "current" {}
+
+module "kms" {
+  source      = "./terraform-modules/kms"
+  alias       = "dlp-key"
+  account_id  = data.aws_caller_identity.current.account_id
+}
+
+module "s3" {
+  source     = "./terraform-modules/s3"
+  bucket     = var.bucket_name
+  kms_key_id = module.kms.key_arn
+}
+
+module "macie" {
+  source      = "./terraform-modules/macie"
+  s3_buckets  = [module.s3.bucket]
+}
+
+module "iam" {
+  source = "./terraform-modules/iam"
+}
+
+module "lambda_remediation" {
+  source        = "./terraform-modules/lambda-remediation"
+  role_arn     = module.iam.lambda_role_arn
+  kms_key_arn  = module.kms.key_arn
+}
+
+module "eventbridge" {
+  source            = "./terraform-modules/eventbridge"
+  lambda_arn        = module.lambda_remediation.lambda_arn
+}
+
+module "s3_dlp" {
+  source      = "./terraform-modules/s3-dlp"
+  bucket_name = module.s3.bucket
+}
+
+module "cloudtrail" {
+  source = "./terraform-modules/cloudtrail"
+}
+
+module "guardduty" {
+  source = "./terraform-modules/guardduty"
+}
+
+module "securityhub" {
+  source = "./terraform-modules/securityhub"
+}
